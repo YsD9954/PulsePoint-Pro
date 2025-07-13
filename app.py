fb5bd55ab9ac8a345902bc977145d93f9875df60
Yash Doke <143099144+YsD9954@users.noreply.github.com>
2025-07-13 12:55:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,9 @@ def predict():
         print("🔥 Server error:", str(e))
         return jsonify({'error': str(e)}), 500
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
